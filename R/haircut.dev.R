796d06077f11ea6b58371d34dcb20104178e4735
olli0601 <oliver.ratmann@ic.ac.uk>
2015-09-02 15:01:57 +0100

    added code to haircut new PANGEA contigs

--- R/haircut.dev.R
+++ R/haircut.dev.R
@@ -459,6 +459,24 @@ pipeline.various<- function()
 {
 	if(1)
 	{		
+		indir.cut	<- paste(DATA, 'contigs_150902_unaligned_cut', sep='/' )
+		indir.raw	<- paste(DATA, 'contigs_150902_unaligned_raw', sep='/' )
+		outdir		<- paste(DATA,'contigs_150902_model150816b',sep='/')
+		batch.n		<- 200
+		tmp			<- data.table(FILE=list.files(indir.raw, pattern='fasta$', recursive=T))
+		tmp[, BATCH:= ceiling(seq_len(nrow(tmp))/batch.n)]
+		tmp			<- tmp[, max(BATCH)]
+		for(batch.id in seq.int(1,tmp))
+		{	
+			cmd			<- cmd.haircut.pipeline(indir.cut, indir.raw, outdir, batch.n=batch.n, batch.id=batch.id)
+			cmd			<- cmd.hpcwrapper(cmd, hpc.nproc= 1, hpc.q='pqeelab', hpc.walltime=1, hpc.mem="5000mb")
+			#cat(cmd)		
+			outfile		<- paste("hrct",paste(strsplit(date(),split=' ')[[1]],collapse='_',sep=''),sep='.')
+			cmd.hpccaller(paste(DATA,"tmp",sep='/'), outfile, cmd)	
+		}
+	}
+	if(0)
+	{		
 		indir.cut	<- paste(DATA, 'contigs_150408_unaligned_cut', sep='/' )
 		indir.raw	<- paste(DATA, 'contigs_150408_unaligned_raw', sep='/' )
 		outdir		<- paste(DATA,'contigs_150408_model150816b',sep='/')
